b5cbc59ba5ede3a9165fc5102acc39bdac9a6cc8
olen-d <olen.daelhousen@outlook.com>
2023-06-22 23:04:11 -0400

    Add feature to return published news stories where the current date and time is greater than the publish date and less than the expiration date

--- app/routes/v1/news-routes.mjs
+++ app/routes/v1/news-routes.mjs
@@ -3,6 +3,7 @@ import * as newsControllers from '../../controllers/v1/news-controllers.mjs'
 const routes = (app, opts, done) => {
   app.delete('/story/:id', { preHandler: app.auth([app.verifyJWT]) }, newsControllers.purgeStory)
   app.get('/', newsControllers.readStoriesPublished)
+  app.get('/active', newsControllers.readStoriesActive)
   app.get('/all', { preHandler: app.auth([app.verifyJWT]) }, newsControllers.readStoriesAll)
   app.get('/story/:id', { preHandler: app.auth([app.verifyJWT]) }, newsControllers.readStoryById)
   app.patch('/story/:id', { preHandler: app.auth([app.verifyJWT]) }, newsControllers.reviseStory)
