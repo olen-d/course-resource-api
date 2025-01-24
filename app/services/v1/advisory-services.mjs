675118bdc3bd9de0b51250afa533d62be993001c
Olen Daelhousen <olen.daelhousen@outlook.com>
2025-01-23 23:10:40 -0500

    Fix issue where the advisory published by id endpoint was returning all advisories

--- app/services/v1/advisory-services.mjs
+++ app/services/v1/advisory-services.mjs
@@ -31,16 +31,8 @@ const deleteAdvisory = async (_db, advisoryId) => {
 
 const readAdvisoriesAll = async (_db, filters) => {
   // TODO: Sanitize filters
-  // TODO: Make this a seperate helper function
-  const mongoFilters = filters.reduce((obj, item) => {
-    const [key] = Object.keys(item)
-    const value = item[key]
-    obj[key] = value
-    return obj
-  }, {})
-
   try {
-    const cursor = await _db.collection('advisories').aggregate([{ $match: mongoFilters }, { $lookup: { from: 'courses', localField: 'coursesAffected', foreignField: '_id', as: 'coursesAffectedLeftJoin' } }, { $project: { facility: 1, from: 1, to: 1, condition: 1, startDate: 1, endDate: 1, isPublished: 1, creatorId: 1, updated: 1, ownerId: 1, coursesAffectedLeftJoin: { _id: 1, title: 1 } } }, { $sort: { 'facility': 1 } }])
+    const cursor = await _db.collection('advisories').aggregate([{ $match: filters }, { $lookup: { from: 'courses', localField: 'coursesAffected', foreignField: '_id', as: 'coursesAffectedLeftJoin' } }, { $project: { facility: 1, from: 1, to: 1, condition: 1, startDate: 1, endDate: 1, isPublished: 1, creatorId: 1, updated: 1, ownerId: 1, coursesAffectedLeftJoin: { _id: 1, title: 1 } } }, { $sort: { 'facility': 1 } }])
     const data = await cursor.toArray()
     return data
   } catch (error) {
